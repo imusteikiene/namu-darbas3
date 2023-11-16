66c0e70427634ea72483304f6276124dea429706
imusteikiene <poskaite.iveta@gmail.com>
2023-11-16 13:54:54 +0200

    init

---
+++ script.js
@@ -0,0 +1,45 @@
+// //pirma uzduotis
+var legalAge = 20;
+var clientAge = 24;
+
+if (clientAge >= legalAge) {
+    alert("Klientas yra pilnametis.leidziama jungtis");
+}
+else {
+    alert("clientas dar nepilnametis")
+}
+
+// // antra uzduotis
+
+var Vardas = "Iveta";
+
+if(Vardas.lenght > 6){
+    crossOriginIsolated.log("Ilgas vardas");
+}else {
+    console.log("nieko neišmeta")
+}
+//  //trecia užduotis
+
+ var amzius = 30; 
+
+if (amzius < 0 || amzius > 100) {
+    console.log("Invalid age");
+} else if (amzius >= 1 && amzius <= 18) {
+    console.log("Child");
+} else if (amzius >= 19 && amzius <= 99) {
+    console.log("Adult");
+} else {
+    console.log("Invalid age");
+}
+//ketrvirta uzduotis
+
+ var car = "Mini";
+ if (car === "VW" || car === "Audi" || car === "Bentley"|| car === "Bugatti" || car === "Lamborgini" || car === "Porshe")
+ { console.log (" automobilis priklauso VW Group");
+ }
+  else if(car === "Bmw"|| car === "Bmw" || car === "Mini" || car === "Rolls-royce") {
+    console.log("automobilis priklauso BMW Group");
+  }
+
+ 
+ 
